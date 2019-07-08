4b15c0cd4c89ec0eaca60128c8efcdc25ebb25d6
stephane <stephane.vieira75@outlook.fr>
2019-07-08 22:21:07 +0200

    Petits exercices avec des variables

---
+++ variable/demo.php
@@ -0,0 +1,11 @@
+<?php
+// Variables
+    $name = 'Stephane';
+    $age = 23;
+    $note = 20;
+    $note2 = 30;
+
+// Instructions
+    echo "Je m'appelle $name , j'ai $age ans.\n";
+    echo "La moyenne d'age d'un developpeur web est de " . (($note + $note2) / 2) . " ans.\n";
+?>
---
+++ variable/demo2.php
@@ -0,0 +1,11 @@
+<?php
+// Variables
+    $prenom = 'Marc';
+    $nom = 'Doe';
+    $note1 = 10;
+    $note2 = 20;
+    $moyenne = ($note1 + $note2) / 2;
+
+// Instructions
+    echo "Bonjour $prenom $nom vous avez eu $moyenne de moyenne.\n";
+?>
