8a51e1b699a2dd3f84ec62c09888ffb709f02716
stephane <stephane.vieira75@outlook.fr>
2019-07-12 01:49:09 +0200

    Systeme de vote majoritaire qui permet de choisir un candidat et lui attribuer une mention

---
+++ challenge/ft_vote_majoritaire/phrase.php
@@ -0,0 +1,8 @@
+<?php
+//Phrase pour les readlines
+$phrase = 
+[
+    'nom' => "Inserer un nom : (Beyonce - Gandalf - Elsa - Chuck Norris - Hermione - Balou) ",
+    'mention' => "Inserer une mention : (excellent - tres bien - bien - assez bien - passable - insuffisant - rejeter) "
+];
+?>
---
+++ challenge/ft_vote_majoritaire/tableau.php
@@ -0,0 +1,84 @@
+<?php
+//  Tableau 
+$tableau = 
+[
+    // Balou
+    [
+        'name' => 'Balou',
+        'mention' => [ 
+            'excellent' => 0, 
+            'tres bien' => 0, 
+            'bien' => 0, 
+            'assez bien' => 0, 
+            'passable' => 0, 
+            'insuffisant' => 0, 
+            'rejeter' => 0
+        ]
+    ],
+    // Hermione
+    [
+        'name' => 'Hermione',
+        'mention' => [ 
+            'excellent' => 0, 
+            'tres bien' => 0, 
+            'bien' => 0, 
+            'assez bien' => 0, 
+            'passable' => 0, 
+            'insuffisant' => 0, 
+            'rejeter' => 0
+        ]
+    ],
+    // Chuck Norris
+    [
+        'name' => 'Chuck-Norris',
+        'mention' => [ 
+            'excellent' => 0, 
+            'tres bien' => 0, 
+            'bien' => 0, 
+            'assez bien' => 0, 
+            'passable' => 0, 
+            'insuffisant' => 0, 
+            'rejeter' => 0
+        ]
+    ],
+    // Elsa
+    [
+        'name' => 'Elsa',
+        'mention' => [ 
+            'excellent' => 0, 
+            'tres bien' => 0, 
+            'bien' => 0, 
+            'assez bien' => 0, 
+            'passable' => 0, 
+            'insuffisant' => 0, 
+            'rejeter' => 0
+        ]
+    ],
+    // Gandalf
+    [
+        'name' => 'Gandalf',
+        'mention' => [ 
+            'excellent' => 0, 
+            'tres bien' => 0, 
+            'bien' => 0, 
+            'assez bien' => 0, 
+            'passable' => 0, 
+            'insuffisant' => 0, 
+            'rejeter' => 0
+        ]
+    ],
+    // Beyonce
+    [
+        'name' => 'Beyonce',
+        'mention' => [ 
+            'excellent' => 0, 
+            'tres bien' => 0, 
+            'bien' => 0, 
+            'assez bien' => 0, 
+            'passable' => 0, 
+            'insuffisant' => 0, 
+            'rejeter' => 0
+        ]
+    ],
+];
+?>
