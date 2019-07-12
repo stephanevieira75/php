60dcab6d84c11a1c74f7b3fdb6af4de957ecfce2
stephane <stephane.vieira75@outlook.fr>
2019-07-12 02:00:59 +0200

    ajout d'un '-' a Chuck-Norris

--- challenge/ft_vote_majoritaire/phrase.php
+++ challenge/ft_vote_majoritaire/phrase.php
@@ -2,7 +2,7 @@
 //Phrase pour les readlines
 $phrase = 
 [
-    'nom' => "Inserer un nom : (Beyonce - Gandalf - Elsa - Chuck Norris - Hermione - Balou) ",
+    'nom' => "Inserer un nom : (Beyonce - Gandalf - Elsa - Chuck-Norris - Hermione - Balou) ",
     'mention' => "Inserer une mention : (excellent - tres bien - bien - assez bien - passable - insuffisant - rejeter) "
 ];
 ?>
