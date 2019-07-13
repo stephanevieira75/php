4a3bcfecf259354f4ad55d62b728db92986e5c05
stephane <stephane.vieira75@outlook.fr>
2019-07-13 20:57:55 +0200

    modification du jeu en un petit formulaire pour composer une glace

--- html/html-1/jeu.php
+++ html/html-1/jeu.php
@@ -1,34 +1,70 @@
 <?php
-//Variables 
-$aDeviner = 150;
-$erreur = null;
-$succes = null;
-$value = null;
+// Tableau des parfums de glace
+$parfums = 
+[
+    'Fraise' => 4,
+    'Vanille' => 3,
+    'Chocolat' => 5
+    
+];
+// Tableau de la taille de la glace
+$cornets = 
+[
+    's' => 2,
+    'm' => 3,
+    'l' => 4,
+    'xl' => 5
+];
+// Tableau des supplements de la glace
+$supplements = 
+[
+    'Pépites de chocolat' => 1,
+    'Chantilly' => 0.5
+];
 
-/* Début de condition :
-Si la variable globale n'a pas d'entrée: */
-if (isset($_POST['chiffre']))
-{
-    $value = (int)$_POST['chiffre'];
-    // Si le chiffre est trop grand :
-    if ($value > $aDeviner)
-    {
-        $erreur = "Votre chiffre est trop grand !";
-    }
-    // Si le chiffre est trop petit :
-    else if ($value < $aDeviner){
-        $erreur = "Votre chiffre est trop petit !";
-    }
-    // Si le chiffre est le bon
-    else
-    {
-        $succes = "Bravo vous avez deviné le chiffre : $aDeviner";
-    }
-
-}
 //On appelle le fichier header.php
 require 'header.php';
+require 'functions.php';
 ?>
+<div class="jumbotron">
+    <h1 class="text-center">Composer votre glace :</h1>
+</div>
+<!-- Formulaire du jeu -->
+<form action="/jeu.php" method="GET">
+
+
+    <h2>Choisi les parfums de ta glace: </h2>
+    <?php foreach ($parfums as $parfum => $prix): ?>
+    <div class="checkbox">
+        <label>
+            <?= checkbox('parfum', $parfum, $_GET) ?>
+            <?= $parfum ?> - <?= $prix ?>€
+        </label>
+    </div>
+    <?php endforeach ?>
+
+    <h2>Choisi la taille de ton cornet: </h2>
+    <?php foreach ($cornets as $cornet => $prix): ?>
+    <div class="radio">
+        <label>
+            <?= radio('cornet', $cornet, $_GET) ?>
+            <?= $cornet ?> - <?= $prix ?>€
+        </label>
+    </div>
+    <?php endforeach ?>
+
+    <h2>Choisi ton supplément: </h2>
+    <?php foreach ($supplements as $supplement => $prix): ?>
+    <div class="checkbox">
+        <label>
+            <?= checkbox('supplement', $supplement, $_GET) ?>
+            <?= $supplement ?> - <?= $prix ?>€
+        </label>
+    </div>
+    <?php endforeach ?>
+
+    <button type="submit" class="btn btn-primary">Composer ma glace</button>
+</form>
 
 <pre>
     <h1>$_GET</h1>
@@ -39,27 +75,6 @@ require 'header.php';
     <h1>$_POST</h1>
     <?php var_dump($_POST); ?>
 </pre>
-
-<?php if ($erreur): ?>
-    <div class="alert alert-danger">
-        <?= $erreur ?>
-    </div>
-<?php elseif ($succes): ?>
-    <div class="alert alert-success">
-        <?= $succes ?>
-    </div>
-<?php endif ?>
-
-<!-- Formulaire du jeu -->
-<form action="/jeu.php" method="POST">
-    <div class="form-group">
-        <!-- Input -->
-        <input type="number" class="form-control" name="chiffre" placeholder="Entre 0 et 1000" value="<?= $value ?>">
-    </div>
-        <!-- Submit -->
-        <button class="btn btn-primary" type="submit">Deviner</button>
-</form>
-
 
  <!-- On appelle le fichier footer.php -->
 <?php require 'footer.php'?>
